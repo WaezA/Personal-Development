43eb78d7fd33b13a901e22d8f1fdfdaf1fde0f86
Waez <72048736+WaezA@users.noreply.github.com>
2021-08-25 21:43:49 +0100

    Beginning of Practice Python Exercises
    Making the starting templates to begin the tasks

---
+++ Python Learning/Python Challenges/Practice Python Exercises.py
@@ -0,0 +1,111 @@
+print ("Practice Python Exercises - https://www.practicepython.org/")
+exercises =["1: Character Input", "2: Odd Or Even", "3: List Less Than Ten", "4: Divisors", "5: List Overlap", "6: String Lists", "7: List Comprehensions", "8: Rock Paper Scissors", "9: Guessing Game One", "10: List Overlap Comprehensions", "11: Check Primality Functions", "12: List Ends", "13: Fibonacci", "14: List Remove Duplicates", "15: Reverse Word Order", "16: Password Generator", "17: Decode A Web Page", "18: Cows And Bulls", "19: Decode A Web Page Two", "20: Element", "21: Write To A File", "22: Read From File", "23: File Overlap", "24: Draw A Game Board", "25: Guessing Game Two", "26: Check Tic Tac Toe", "27: Tic Tac Toe Draw", "28: Max Of Three", "29: Tic Tac Toe Game", "30: Pick Word", "31: Guess Letters", "32: Hangman", "33: Birthday Dictionaries", "34: Birthday Json" ,"35: Birthday Months", "36: Birthday"]
+print (exercises)
+
+def task_1():
+    print (exercises[0])
+
+def task_2():
+    print (exercises[1])
+
+def task_3():
+    print (exercises[2])
+
+def task_4():
+    print (exercises[3])
+
+def task_5():
+    print (exercises[4])
+
+def task_6():
+    print (exercises[5])
+
+def task_7():
+    print (exercises[6])
+
+def task_8():
+    print (exercises[7])
+
+def task_9():
+    print (exercises[8])
+
+def task_10():
+    print (exercises[9])
+
+def task_11():
+    print (exercises[10])
+
+def task_12():
+    print (exercises[11])
+
+def task_13():
+    print (exercises[12])
+
+def task_14():
+    print (exercises[13])
+
+def task_15():
+    print (exercises[14])
+
+def task_16():
+    print (exercises[15])
+
+def task_17():
+    print (exercises[16])
+
+def task_18():
+    print (exercises[17])
+
+def task_19():
+    print (exercises[18])
+
+def task_20():
+    print (exercises[19])
+
+def task_21():
+    print (exercises[20])
+
+def task_22():
+    print (exercises[21])
+
+def task_23():
+    print (exercises[22])
+
+def task_24():
+    print (exercises[23])
+
+def task_25():
+    print (exercises[24])
+
+def task_26():
+    print (exercises[25])
+
+def task_27():
+    print (exercises[26])
+
+def task_28():
+    print (exercises[27])
+
+def task_29():
+    print (exercises[28])
+
+def task_30():
+    print (exercises[29])
+
+def task_31():
+    print (exercises[30])
+
+def task_32():
+    print (exercises[31])
+
+def task_33():
+    print (exercises[32])
+
+def task_34():
+    print (exercises[33])
+
+def task_35():
+    print (exercises[34])
+
+def task_36():
+    print (exercises[35])
