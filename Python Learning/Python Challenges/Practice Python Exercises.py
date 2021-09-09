fbb0246117d48dbe70877234af779fbcd34d8979
Waez <72048736+WaezA@users.noreply.github.com>
2021-09-09 01:32:38 +0100

    Divisors Task 4 Completed
    Task complete

--- Python Learning/Python Challenges/Practice Python Exercises.py
+++ Python Learning/Python Challenges/Practice Python Exercises.py
@@ -1,3 +1,5 @@
+import random
+
 print ("Practice Python Exercises - https://www.practicepython.org/")
 exercises =["1: Character Input", "2: Odd Or Even", "3: List Less Than Ten", "4: Divisors", "5: List Overlap", "6: String Lists", "7: List Comprehensions", "8: Rock Paper Scissors", "9: Guessing Game One", "10: List Overlap Comprehensions", "11: Check Primality Functions", "12: List Ends", "13: Fibonacci", "14: List Remove Duplicates", "15: Reverse Word Order", "16: Password Generator", "17: Decode A Web Page", "18: Cows And Bulls", "19: Decode A Web Page Two", "20: Element", "21: Write To A File", "22: Read From File", "23: File Overlap", "24: Draw A Game Board", "25: Guessing Game Two", "26: Check Tic Tac Toe", "27: Tic Tac Toe Draw", "28: Max Of Three", "29: Tic Tac Toe Game", "30: Pick Word", "31: Guess Letters", "32: Hangman", "33: Birthday Dictionaries", "34: Birthday Json" ,"35: Birthday Months", "36: Birthday"]
 print (exercises)
@@ -87,7 +89,21 @@ def task_3():
 
 def task_4():
     print (exercises[3])
-
+    mod = input("Enter a number: ")
+    factors = list()
+    for i in range (int(mod)+1):
+        factors.append(int(i))
+        i += 1
+    factors.remove(0)
+    print ("Factors for " + mod + " are:")
+    
+    for i in range (int(mod)):
+        if (int(mod)%int(factors[i])) ==0:
+            print (factors[i])
+            i += 1
+        else:
+            i += 1
+            continue
 def task_5():
     print (exercises[4])
 
