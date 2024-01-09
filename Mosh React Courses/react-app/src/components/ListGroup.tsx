9ea85af169e067c58963858224d6b0594cf07ce1
Waez <72048736+WaezA@users.noreply.github.com>
2024-01-09 23:24:27 +0000

    Update ListGroup.tsx

--- Mosh React Courses/react-app/src/components/ListGroup.tsx
+++ Mosh React Courses/react-app/src/components/ListGroup.tsx
@@ -1,14 +1,16 @@
 function ListGroup() {
   return (
-    <ul className="list-group">
-      <li className="list-group-item disabled" aria-disabled="true">
-        A disabled item
-      </li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <div>
+      <ul className="list-group">
+        <li className="list-group-item disabled" aria-disabled="true">
+          A disabled item
+        </li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </div>
   );
 }
 
